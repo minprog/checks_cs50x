f366c5c14fc0946e46f902030fc6cdd9cc9cae78
Quinten <qvdpost@gmail.com>
2019-03-07 16:54:51 +0100

    number of args

--- adventure/check50/__init__.py
+++ adventure/check50/__init__.py
@@ -68,13 +68,26 @@ class Adventure(Checks):
                 check50.copy(path, dest)
 
     @check("exists")
-    def takes_arguments(self):
+    def handles_arguments(self):
         """Starting adventure with command line argument 'Tiny'."""
         try:
             self.spawn_tiny().stdout(re.escape(room_1_description), str_output=room_1_description)
         except Error as error:
             raise Error(rationale=f"Expected the description of initial "
                                   f"room when Adventure starts.\n    {error}")
+
+    @check("handles_arguments")
+    def rejects_incorrect_arguments(self):
+        """Rejects incorrect amount of arguments or wrong filename."""
+        try:
+            self.spawn("python3 adventure.py").exit(1)
+        except Error as error:
+            raise Error(rationale=f"Expected an exit code of 1. Not {error}")
+
+        try:
+            self.spawn("python3 adventure.py Tiny CS50").exit(1)
+        except Error as error:
+            raise Error(rationale=f"Expected an exit code of 1. Not {error}")
 
         try:
             self.spawn("python3 adventure.py CS50").exit(1)
