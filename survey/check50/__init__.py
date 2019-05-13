4b715b8a2e1e3559ac6021c637d318c459ee0adf
Jelle van Assema <jelle.van.assema@gmail.com>
2019-05-13 17:36:52 +0200

    Auto unzip in finance & survey (#3)
    * unpacker for survey
    * Survey -> Finance

--- survey/check50/__init__.py
+++ survey/check50/__init__.py
@@ -1,5 +1,17 @@
 from check50 import *
 from functools import wraps
+import subprocess
+import os
+import shutil
+
+
+ZIPNAME = "survey.zip"
+
+REQUIRED = ["application.py",
+            "templates/layout.html",
+            "templates/form.html",
+            "templates/error.html",
+            "static/styles.css"]
 
 
 def helper(f):
@@ -11,10 +23,41 @@ def helper(f):
     return wrapper
 
 
+def remove_all_but(filename):
+    for f in set(os.listdir(".")) - {filename}:
+        if os.path.isdir(f):
+            shutil.rmtree(f)
+        else:
+            os.remove(f)
+
+
+def unpack(filename):
+    subprocess.call(["unzip", filename], stdout=subprocess.PIPE)
+
+
+def goto(filename):
+    contents = os.listdir(".")
+
+    # Traverse through dir until filename is found
+    while filename not in contents:
+        dirs = [c for c in contents if not c.startswith(".") and os.path.isdir(c)]
+
+        # If there's more than 1 dir, fail
+        if len(dirs) != 1:
+            return False
+
+        # Goto only dir
+        dir = dirs[0]
+        contents = os.listdir(dir)
+        for c in contents:
+            shutil.move(os.path.join(dir, c), ".")
+        os.rmdir(dir)
+
+    return True
+
+
 class Survey(Checks):
-
     APP = "application.py"
-
 
     @helper
     def get_content(self, route, tag="input"):
@@ -25,7 +68,14 @@ class Survey(Checks):
     @check()
     def exists(self):
         """application.py exists"""
-        self.require("application.py")
+        if ZIPNAME in os.listdir("."):
+            remove_all_but(ZIPNAME)
+            unpack(ZIPNAME)
+
+            if not goto(REQUIRED[0]):
+                raise Error(f"Could not find {REQUIRED[0]} in .zip")
+
+        self.require(*REQUIRED)
 
 
     @check("exists")
@@ -80,3 +130,7 @@ class Survey(Checks):
         if len(selects) < 1 or \
             any((len(select.find_all("option")) < 2 for select in selects)):
             raise Error("expected one or more select menus, each with two or more options")
+
+
+if ZIPNAME in os.listdir("."):
+    Survey.exists.__doc__ = f"Extracted {ZIPNAME} and {REQUIRED[0]} exists"
